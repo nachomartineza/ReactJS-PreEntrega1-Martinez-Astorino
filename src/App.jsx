649caa18b546683f8f26e59fa1802d97f5fc7018
nachomartineza <nachomartinezps4@gmail.com>
2023-05-10 11:28:56 -0300

    probadno

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p><b>react js nachito</b></p>
         <a
           className="App-link"
           href="https://reactjs.org"
